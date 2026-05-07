7dd1dc320e6a5380f679850165e7808c219bc56d
LuisEdu126475 <Alucard66khs@gmail.com>
2026-05-06 21:27:49 -0300

    Aula do dia 06/05/2026

---
+++ Atividades/atividade_11/js/script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault()
+
+    let altura = document.querySelector("#altura").value
+    let peso = document.querySelector("#peso").value
+
+    altura = parseFloat(altura)
+    peso = parseFloat(peso)
+
+    let imc = peso / (altura * altura)
+
+    imc = imc.toFixed(2)
+
+    const resultado = document.querySelector("#result")
+
+    if(imc < 18.5){
+        resultado.innerHTML = `Magreza - IMC: ${imc}`
+    }
+
+    else if(imc < 25){
+        resultado.innerHTML = `Peso normal - IMC: ${imc}`
+    }
+
+    else if(imc < 30){
+        resultado.innerHTML = `Sobrepeso - IMC: ${imc}`
+    }
+
+    else{
+        resultado.innerHTML = `Obesidade - IMC: ${imc}`
+    }
+
+})
