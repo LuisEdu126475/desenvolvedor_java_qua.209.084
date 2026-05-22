aef1c6f1c6213aea7a4ae2cb2a7137e82b8d65d2
LuisEdu126475 <Alucard66khs@gmail.com>
2026-05-21 21:45:43 -0300

    Aula do dia 21/05/2026

--- Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/models/Categoria.java
+++ Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/models/Categoria.java
@@ -1,8 +1,12 @@
 package com.crud.javalanches.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
@@ -19,6 +23,11 @@ private static long serialVersionUID = 1L;
 private long codigoCliente;
 @Column(unique = true, nullable = false)
 private String nomeCategoria;
+
+@OneToMany(mappedBy = "categoria")
+
+private List<Produto> produtos = new ArrayList<>();
+
 
 
     public Categoria() {
@@ -38,6 +47,13 @@ private String nomeCategoria;
 
     public void setNomeCategoria(String nomeCategoria) {
         this.nomeCategoria = nomeCategoria;
+    }
+     public List<Produto> getProdutos() {
+        return this.produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
 }
